caa3a19826c4f57c5cbf899306235d17558125f2
Alexandr Popolitov <popolit@gmail.com>
2015-10-23 13:00:41 +0200

    Export YIELD as well

--- package.lisp
+++ package.lisp
@@ -2,7 +2,7 @@
 
 (defpackage #:cl-itertools
   (:use #:cl #:iterate #:cl-coroutine)
-  (:export #:in-coro #:with-arg #:in-it #:defiter
+  (:export #:in-coro #:with-arg #:in-it #:defiter #:yield
 	   #:coexit! #:lambda-coro
 	   #:icount #:icycle #:irepeat #:ichain #:icompress #:idropwhile #:igroupby
 	   #:ifilter #:ifilterfalse #:islice #:imap #:istarmap #:itee
